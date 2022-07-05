d45f977c0e80b7aac1f5a6d5e0c3fb66e24654c5
Jiajie <jiajiehe@yahoo.com>
2022-07-04 21:28:01 -1000

    added conditions so that the timeblocks are color-coded

--- script.js
+++ script.js
@@ -1,18 +1,126 @@
 // display current day
 var today = moment();
-$(`#currentDay`).text(today.format(`dddd, MMMM Do`))
+
+function displayTime (){
+    var time = moment().format(`dddd, MMMM Do YYYY, h:mm:ss a`);
+    $(`#currentDay`).text(time)
+
+}
+
+setInterval(displayTime, 1000);
+
+// color-code past, present, future
+// grab textarea give them value
+var nineAmEl = $(`#nineAm`)
+nineAmEl = 09
+// create condition for present future past
+if (nineAmEl == today.format(`HH`)) {
+    $(`#nine`).addClass(`present`)
+} else if (nineAmEl > today.format(`HH`)) {
+    $(`#nine`).addClass(`future`)
+} else {
+    $(`#nine`).addClass(`past`)
+}
+
+var tenAmEl = $(`#tenAm`)
+tenAmEl = 10
+
+if (tenAmEl == today.format(`HH`)) {
+    $(`#ten`).addClass(`present`)
+} else if (tenAmEl > today.format(`HH`)) {
+    $(`#ten`).addClass(`future`)
+} else {
+    $(`#ten`).addClass(`past`)
+}
+
+var elevenAmEl = $(`#elevenAm`)
+elevenAmEl = 11
+
+if (elevenAmEl == today.format(`HH`)) {
+    $(`#eleven`).addClass(`present`)
+} else if (elevenAmEl > today.format(`HH`)) {
+    $(`#eleven`).addClass(`future`)
+} else {
+    $(`#eleven`).addClass(`past`)
+}
+
+var twelvePmEl = $(`#twelvePm`)
+twelvePmEl = 12
+
+if (twelvePmEl == today.format(`HH`)) {
+    $(`#twelve`).addClass(`present`)
+} else if (twelvePmEl > today.format(`HH`)) {
+    $(`#twelve`).addClass(`future`)
+} else {
+    $(`#twelve`).addClass(`past`)
+}
+
+var onePmEl = $(`#onePm`)
+onePmEl = 13
+
+if (onePmEl == today.format(`HH`)) {
+    $(`#thirteen`).addClass(`present`)
+} else if (onePmEl > today.format(`HH`)) {
+    $(`#thirteen`).addClass(`future`)
+} else {
+    $(`#thirteen`).addClass(`past`)
+}
+
+var twoPmEl = $(`#twoPm`)
+twoPmEl = 14
+
+if (twoPmEl == today.format(`HH`)) {
+    $(`#fourteen`).addClass(`present`)
+} else if (twoPmEl > today.format(`HH`)) {
+    $(`#fourteen`).addClass(`future`)
+} else {
+    $(`#fourteen`).addClass(`past`)
+}
+
+var threePmEl = $(`#threePm`)
+threePmEl = 15
+
+if (threePmEl == today.format(`HH`)) {
+    $(`#fifteen`).addClass(`present`)
+} else if (threePmEl > today.format(`HH`)) {
+    $(`#fifteen`).addClass(`future`)
+} else {
+    $(`#fifteen`).addClass(`past`)
+}
+
+var fourPmEl = $(`#fourPm`)
+fourPmEl = 16
+
+if (fourPmEl == today.format(`HH`)) {
+    $(`#sixteen`).addClass(`present`)
+} else if (fourPmEl > today.format(`HH`)) {
+    $(`#sixteen`).addClass(`future`)
+} else {
+    $(`#sixteen`).addClass(`past`)
+}
+
+var fivePmEl = $(`#fivePm`)
+fivePmEl = 17
+
+if (fivePmEl == today.format(`HH`)) {
+    $(`#seventeen`).addClass(`present`)
+} else if (fivePmEl > today.format(`HH`)) {
+    $(`#seventeen`).addClass(`future`)
+} else {
+    $(`#seventeen`).addClass(`past`)
+}
 
 // save input to local storage
 // grab element from html
-var nineEl = document.querySelector(`#nine`)
-var tenEl = document.querySelector(`#ten`)
-var elevenEl = document.querySelector(`#eleven`)
-var twelveEl = document.querySelector(`#twelve`)
-var thirteenEl = document.querySelector(`#thirteen`)
-var fourteenEl = document.querySelector(`#fourteen`)
-var fifteenEl = document.querySelector(`#fifteen`)
-var sixteenEl = document.querySelector(`#sixteen`)
-var seventeenEl = document.querySelector(`#seventeen`)
+var nineEl = document.querySelector(`#nine`);
+var tenEl = document.querySelector(`#ten`);
+var elevenEl = document.querySelector(`#eleven`);
+var twelveEl = document.querySelector(`#twelve`);
+var thirteenEl = document.querySelector(`#thirteen`);
+var fourteenEl = document.querySelector(`#fourteen`);
+var fifteenEl = document.querySelector(`#fifteen`);
+var sixteenEl = document.querySelector(`#sixteen`);
+var seventeenEl = document.querySelector(`#seventeen`);
 
 // grab value from local storage
 var nine = localStorage.getItem(`nine`);
@@ -44,6 +152,7 @@ var saveBtn = document.querySelectorAll(`.saveBtn`)
 saveBtn.forEach(btn => {
     btn.addEventListener(`click`, (event) => {
         
+        // grab the content from each textarea
         var nineEl = document.querySelector(`#nine`).value;
         var tenEl = document.querySelector(`#ten`).value;
         var elevenEl = document.querySelector(`#eleven`).value;
@@ -54,6 +163,7 @@ saveBtn.forEach(btn => {
         var sixteenEl = document.querySelector(`#sixteen`).value;
         var seventeenEl = document.querySelector(`#seventeen`).value;
         
+        // set name and value to local storage
         localStorage.setItem(`nine`, nineEl);
         localStorage.setItem(`ten`, tenEl);
         localStorage.setItem(`eleven`, elevenEl);
@@ -68,19 +178,3 @@ saveBtn.forEach(btn => {
     })
 
 })
-
-
-
-
-
-
-
-//TODO: color-code past, present, future
-
-// if (hour == today.format(`h a`)) {
-//     $(`textarea`).addClass(`present`)
-// } else if (hour > today.format(`h`)) {
-//     $(`textarea`).addClass(`future`)
-// } else {
-//     $(`textarea`).addClass(`past`)
-// }
